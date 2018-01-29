bf6b70dc62fdb00fef6d39e0e02ec2ebefe106b7
Henry_Quant <leebisu@gmail.com>
2018-01-29 13:49:03 +0900

    yr_plot

---
+++ R/yr_plot.R
@@ -0,0 +1,19 @@
+#' Plot Yearly Return
+#'
+#' yr_plot function will plot yearly return. It require "lubridate" package.
+#'
+#' @param Return Return Data
+#' @return Plot Yearly return
+#' @examples
+#' \dontrun{
+#'   ret = asset_data
+#'   yr_plot(ret)
+#'   }
+#' @export
+yr_plot = function(Return) {
+  year_ret = as.data.frame(apply.yearly(Return, Return.cumulative))
+  rownames(year_ret) = year(rownames(year_ret))
+  barplot(t(year_ret), col=1:ncol(Return), beside = TRUE, las = 2 )
+  if (ncol(Return) > 1)
+    legend('topright', colnames(Return), col=1:ncol(Return), lty=NA, lwd = 1, pch = c(15), cex = 0.8)
+}

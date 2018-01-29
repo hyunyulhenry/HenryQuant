180b80932e71257557b686d98e8ef178709fd1e2
Henry_Quant <leebisu@gmail.com>
2018-01-29 12:13:33 +0900

    commit

---
+++ R/ipak.R
@@ -0,0 +1,18 @@
+#' Download & Load Packages
+#'
+#' ipak function will Download unpacked packages, then load packages.
+#'
+#' @param pkg List of packages to install.
+#' @return Download & Install packages
+#' @examples
+#' \dontrun{
+#'   pkg = c("PerformanceAnalytics", "quantmod")
+#'   ipak(pkg)
+#'   }
+#' @export
+ipak = function(pkg){
+  new.pkg = pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg))
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}

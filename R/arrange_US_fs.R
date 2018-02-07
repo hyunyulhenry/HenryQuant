9f9ced832d25fe7b3b64e7b7641a1d1cf90a6dc6
Henry_Quant <leebisu@gmail.com>
2018-02-07 09:44:22 +0900

    git

--- R/arrange_US_fs.R
+++ R/arrange_US_fs.R
@@ -13,8 +13,14 @@
 #' @export
 arrange_US_fs = function() {
 
-  ticker = get_US_ticker()
   fs_name = "US_fs"
+  if (dir.exists(fs_name) == FALSE) {
+    warning("You Need Download Financial Statement Data. Please execute get_US_fs() first")
+    break
+  }
+
+  ticker = get_US_ticker()
+
   test = read.csv(paste0(getwd(),"/",fs_name,"/",ticker[1,1],"_fs.csv"))
   m = nrow(test) - 1
   n = ncol(test) - 1
@@ -23,36 +29,30 @@ arrange_US_fs = function() {
 
   fs_list = rep( list(list()), m )
 
-  if (dir.exists(fs_name) == FALSE) {
-    warning("You Need Download Financial Statement Data. Please execute get_US_fs() first")
-    break
-  } else {
-
-    for (i in 1 : nrow(ticker)) {
-      tryCatch({
-        temp = matrix(NA, m, n) %>% as.data.frame %>% set_colnames(fs_colnames)
-        temp = read.csv(paste0(getwd(),"/",fs_name,"/",ticker[i,1],"_fs.csv"))
-        temp = temp[!duplicated(temp[,1]),] %>% set_rownames(NULL) %>%
-          column_to_rownames(var = "X")
-        if (ncol(temp) != 4) {
-          temp = cbind(temp, matrix(NA, nrow(temp), (4-ncol(temp)) ))
-        }
-        colnames(temp) = fs_colnames
-      }, error = function(e){})
-
-    for (j in 1 : m) {
-      if (i == 1) {
-        fs_list[[j]] = temp[j, ] %>% set_rownames(ticker[i, 1])
-        } else {
-        fs_list[[j]] = rbind(fs_list[[j]], temp[j, ] %>% set_rownames(ticker[i, 1]))
-        }
+  for (i in 1 : nrow(ticker)) {
+    tryCatch({
+      temp = matrix(NA, m, n) %>% as.data.frame %>% set_colnames(fs_colnames)
+      temp = read.csv(paste0(getwd(),"/",fs_name,"/",ticker[i,1],"_fs.csv"))
+      temp = temp[!duplicated(temp[,1]),] %>% set_rownames(NULL) %>%
+        column_to_rownames(var = "X")
+      if (ncol(temp) != 4) {
+        temp = cbind(temp, matrix(NA, nrow(temp), (n-ncol(temp)) ))
       }
+      colnames(temp) = fs_colnames
+    }, error = function(e){})
 
-      if ((i %% 50) == 0) { print(paste0(round((i / nrow(ticker)) * 100,2)," %")) }
+  for (j in 1 : m) {
+    if (i == 1) {
+      fs_list[[j]] = temp[j, ] %>% set_rownames(ticker[i, 1])
+      } else {
+      fs_list[[j]] = rbind(fs_list[[j]], temp[j, ] %>% set_rownames(ticker[i, 1]))
+      }
     }
-    names(fs_list) = fs_account
-    write.csv(fs_list, "fs_list_US.csv")
-    return(fs_list)
+
+    if ((i %% 50) == 0) { print(paste0(round((i / nrow(ticker)) * 100,2)," %")) }
   }
+  names(fs_list) = fs_account
+  write.csv(fs_list, "fs_list_US.csv")
+  return(fs_list)
 
 }

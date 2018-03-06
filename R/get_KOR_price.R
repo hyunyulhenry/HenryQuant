503067a62d878055fd301d5d34b02e70ff8f187b
Henry_Quant <leebisu@gmail.com>
2018-03-06 15:33:40 +0900

    git

--- R/get_KOR_price.R
+++ R/get_KOR_price.R
@@ -75,6 +75,12 @@ get_KOR_price = function(num_limit = 25) {
     }
 
     # If there is no data or crawling error, make it as empty price
+
+    price = do.call(rbind, price) %>% as.xts %>% set_colnames(ticker[i, 1])
+    price = price[!duplicated(index(price)), ]
+
+    }, error = function(e){})
+
     if (length(price) == 0) {
       price = list()
       price[[1]] = xts(matrix(NA, 1, 1), order.by = today())
@@ -86,12 +92,6 @@ get_KOR_price = function(num_limit = 25) {
       price[[1]] = xts(matrix(NA, 1, 1), order.by = today())
       warning(paste0("Check Ticker"," ",ticker[i, 1]))
     }
-
-
-    price = do.call(rbind, price) %>% as.xts %>% set_colnames(ticker[i, 1])
-    price = price[!duplicated(index(price)), ]
-
-    }, error = function(e){})
 
     write.csv(as.matrix(price),paste0(getwd(),"/","KOR_price","/",ticker[i,1],"_price.csv"))
     print(paste0(ticker[i, 1]," ",ticker[i,2]," ",round(i / nrow(ticker) * 100,3),"%"))

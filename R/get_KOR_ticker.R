8213704e68a83d558ef481891387530060003956
Henry_Quant <leebisu@gmail.com>
2018-04-21 14:00:16 +0900

    git

--- R/get_KOR_ticker.R
+++ R/get_KOR_ticker.R
@@ -12,44 +12,42 @@
 #' @export
 get_KOR_ticker = function() {
 
-  ticker = c('STK', 'KSQ')
+  d = function(market, date) {
+    gen_otp_url = "http://marketdata.krx.co.kr/contents/COM/GenerateOTP.jspx"
+    gen_otp_data = list(name = "fileDown", filetype = "csv",
+                         url = "MKD/04/0404/04040200/mkd04040200_01", market_gubun = market,
+                         indx_ind_cd = "", sect_tp_cd = "", schdate = date, pagePath = "/contents/MKD/04/0404/04040200/MKD04040200.jsp")
+    otp = POST(gen_otp_url, query = gen_otp_data)
+    otp_content = content(otp, "text")
+    down_url = "http://file.krx.co.kr/download.jspx"
+    down_data = list(code = otp_content)
+    html = POST(down_url, query = down_data)
+    down = getURL(html$url, .encoding = "UTF-8")
+    temp = read_csv(down)
+    return(temp)
+  }
+
+  ticker = c("STK", "KSQ")
+  date = gsub("-", "", Sys.Date())
   data = list()
 
   for (i in 1:2) {
 
-    gen_otp_url = 'http://marketdata.krx.co.kr/contents/COM/GenerateOTP.jspx'
+    tryCatch({
+      d(ticker[i], date)
+    }, error = function(e) {
+      date <<- gsub("-", "", Sys.Date()-1)
+    })
 
-    gen_otp_data <- list(
-      name = 'fileDown',
-      filetype = 'csv',
-      url = 'MKD/04/0404/04040200/mkd04040200_01',
-      'market_gubun' = ticker[i],
-      'indx_ind_cd' = '',
-      'sect_tp_cd' = '',
-      'schdate' = gsub("-","",Sys.Date()),
-      'pagePath' = '/contents/MKD/04/0404/04040200/MKD04040200.jsp'
-    )
+    x = d(ticker[i], date)
 
-    otp = POST(gen_otp_url, query = gen_otp_data)
-    otp_content  = content(otp,"text")
-
-    down_url = 'http://file.krx.co.kr/download.jspx'
-    down_data = list(
-      code = otp_content
-    )
-
-    html = POST(down_url, query = down_data)
-
-    down = getURL(html$url, .encoding = "UTF-8")
-    temp = read_csv(down)
-    data[[i]] = as.data.frame(temp) %>% column_to_rownames(var = colnames(temp)[1])
+    data[[i]] = as.data.frame(x) %>% column_to_rownames(var = colnames(x)[1])
   }
 
   data = do.call(rbind, data)
-  data = data[,1:2]
+  data = data[, 1:2]
   Sys.setlocale("LC_ALL", "Korean")
-
   write.csv(data, "KOR_ticker_list.csv")
   return(data)
+  }
 
-}

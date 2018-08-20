a8ec07ae0db6cdae243ff5cc62eeab270eeace15
hyunyulhenry <leebisu@gmail.com>
2018-08-20 23:18:28 +0900

    update (2018.08.20)

--- R/get_KOR_fs2.R
+++ R/get_KOR_fs2.R
@@ -1,3 +1,12 @@
+#' This Function is integrated to get_KOR_fs
+#' @return nothing
+#'
+#' @examples
+#' \dontrun{
+#'   get_KOR_fs2()
+#'   }
+#' @export
+
 get_KOR_fs2 = function() {
 
   print("This Function is integrated to get_KOR_fs")

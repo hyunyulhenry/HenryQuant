f4a149ba9b5215bd98c58818a3ad89df7f7aedd0
Henry_Quant <leebisu@gmail.com>
2018-09-12 13:01:35 +0900

    update(2018.09.12)

--- R/get_KOR_price.R
+++ R/get_KOR_price.R
@@ -6,7 +6,7 @@
 #' It will aumomatically save individual stock prices and
 #' combined prices for csv types
 #'
-#' @param num_limit number of days to download. 1 for 30 days
+#' @param src Download from 'yahoo' or 'daum'
 #'
 #' @importFrom utils write.csv
 #' @importFrom xts xts as.xts
@@ -16,20 +16,26 @@
 #' @importFrom httr GET
 #' @importFrom xml2 read_html
 #' @importFrom rvest html_node
+#' @importFrom quantmod Cl getSymbols
 #' @examples
 #' \dontrun{
-#'  get_KOR_price(num_limit = 10)
+#'  get_KOR_price()
 #'  }
 #' @export
-get_KOR_price = function(num_limit = 10) {
+get_KOR_price = function(src = "yahoo") {
 
   folder_name = "KOR_price"
-  ticker = get_KOR_ticker()
-
   ifelse(dir.exists(folder_name), FALSE, dir.create(folder_name))
 
-  # Cleansing #
+  ticker = get_KOR_ticker()
 
+  ticker_name = function(src) {
+    if (src == "yahoo") {name = paste0(ticker[i, 1], ".", ticker[i, 'market'])}
+    if (src == "daum") {name = ticker[i, 1]}
+    return(name)
+  }
+
+  # Cleansing #
   data_cleansing = function(data_table) {
     data_table = data_table[[1]][,c(1,5)]
     data_table[data_table == ""] = NA
@@ -45,54 +51,65 @@ get_KOR_price = function(num_limit = 10) {
     return(data_table)
   }
 
-  # Crawling #
+  for(i in 1 : nrow(ticker) ) {
+    name = ticker_name(src)
 
-  for (i in 1 : nrow(ticker)) {
-
-    name = ticker[i, 1] %>% as.character()
-    price_list = list(xts(NA, order.by = Sys.Date()))
-
-    if(file.exists(paste0(getwd(),"/",folder_name,"/",name,"_price.csv")) == TRUE){
+    if(file.exists(paste0(folder_name,"/",name,"_price.csv")) == TRUE){
       next
     } else {
 
-    tryCatch({
+      if (src == "yahoo") {
+        price = xts(NA, order.by = Sys.Date())
 
-      for (j in 1:10) {
-        url = paste0("http://finance.daum.net/item/quote_yyyymmdd_sub.daum?page=",
-                     j,"&code=",name,"&modify=1")
-
-        Sys.setlocale("LC_ALL", "English")
-        data = GET(url)
-        data_table = read_html(data) %>%
-          html_table()
-        Sys.setlocale("LC_ALL", "Korean")
-
-        price_list[[j]] = data_cleansing(data_table)
-        Sys.sleep(0.5)
+        tryCatch({
+          price = Cl(getSymbols(name, auto.assign = FALSE))
+        }, error = function(e) {
+          print(paste0("Error in Ticker: ", name))
+        })
       }
 
-    }, error = function(e) {
-      warning(paste0("Error in Ticker: ", name))}
-    )
+      if (src == "daum") {
+        price = list(xts(NA, order.by = Sys.Date()))
 
-    price_list = do.call(rbind, price_list)
-    price_list = price_list[!duplicated(index(price_list))]
-    price_list = na.locf(price_list)
+        tryCatch({
 
-    write.csv(data.frame(price_list),paste0(getwd(),"/",folder_name,"/",name,"_price.csv"))
-    print(paste0(name," ",ticker[i,2]," ",round(i / nrow(ticker) * 100,3),"%"))
+          for (j in 1:10) {
+            url = paste0("http://finance.daum.net/item/quote_yyyymmdd_sub.daum?page=",
+                         j,"&code=",name,"&modify=1")
 
-    Sys.sleep(3)
+            Sys.setlocale("LC_ALL", "English")
+            data = GET(url)
+            data_table = read_html(data) %>%
+              html_table()
+            Sys.setlocale("LC_ALL", "Korean")
+
+            price[[j]] = data_cleansing(data_table)
+            Sys.sleep(0.5)
+          }
+
+        }, error = function(e) {
+          warning(paste0("Error in Ticker: ", name))}
+        )
+        price = do.call(rbind, price)
+      }
+
+      price = price[!duplicated(index(price))]
+      price = na.locf(price)
+
+      write.csv(as.matrix(price),paste0(getwd(),"/",folder_name,"/",name,"_price.csv"))
+      print(paste0(ticker[i, 1]," ",ticker[i,2]," ",round(i / nrow(ticker) * 100,3),"%"))
+
+      Sys.sleep(3)
+
     }
   }
 
-    print("Data download is complete. Data binding is in progress.")
+  print("Data download is complete. Data binding is in progress.")
 
   # Arrange #
   price_list = list()
   for (i in 1 : nrow(ticker)){
-    name = ticker[i, 1]
+    name = ticker_name(src)
     price_list[[i]] = as.xts(read.csv(paste0(getwd(),"/",folder_name,"/",name,"_price.csv"), row.names = 1))
     if ((i %% 10) == 0) { print(paste0("Binding Price: ", round((i / nrow(ticker)) * 100,2)," %")) }
   }

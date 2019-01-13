f67437e4323cc4bba1fd2f1fc185bd421102bdd3
hyunyulhenry <leebisu@gmail.com>
2019-01-13 18:18:21 +0900

    거래소 데이터 변경

--- R/get_KOR_ticker.R
+++ R/get_KOR_ticker.R
@@ -7,7 +7,7 @@
 #' @importFrom utils write.csv tail
 #' @importFrom rvest html_text html_nodes html_attr
 #' @importFrom xml2 read_html
-#' @importFrom httr add_headers POST
+#' @importFrom httr add_headers POST write_disk
 #' @importFrom readr read_csv
 #' @examples
 #' \dontrun{
@@ -19,51 +19,78 @@ get_KOR_ticker = function(src = 'krx') {
   print("Ticker Download")
   data = list()
 
+  chr_to_numeric = function(x) {
+    x = gsub(',', '', x) %>% as.numeric
+    return(x)
+  }
+
   # Download form krx
   if (src == 'krx') {
 
-    down_data = function(market, date) {
+    gen_otp_url = "http://marketdata.krx.co.kr/contents/COM/GenerateOTP.jspx"
+    down_url = "http://file.krx.co.kr/download.jspx"
 
-      gen_otp_url = "http://marketdata.krx.co.kr/contents/COM/GenerateOTP.jspx"
+    # 산업별 현황 다운로드
+    down_sector = function(date) {
       gen_otp_data = list(name = "fileDown", filetype = "csv",
-                          url = "MKD/04/0404/04040200/mkd04040200_01", market_gubun = market,
-                          indx_ind_cd = "", sect_tp_cd = "", schdate = date, pagePath = "/contents/MKD/04/0404/04040200/MKD04040200.jsp")
-      otp = POST(gen_otp_url, query = gen_otp_data)
-      otp_content = read_html(otp) %>% html_text()
+                          url = "MKD/03/0303/03030103/mkd03030103", tp_cd = 'ALL',
+                          date = date, lang = 'ko',
+                          pagePath = "/contents/MKD/03/0303/03030103/MKD03030103.jsp")
+      otp = POST(gen_otp_url, query = gen_otp_data) %>%
+        read_html() %>% html_text()
+      down_data = list(code = otp)
 
-      down_url = "http://file.krx.co.kr/download.jspx"
-      down_data = list(code = otp_content)
+      down = POST(down_url, query = down_data,
+                  add_headers(referer = gen_otp_url),
+                  write_disk(tf <- tempfile(fileext = '.csv')))
+      data_sector = read_csv(tf)
+
+      data_sector = data_sector[, c(1:4, 7)]
+      write.csv(data_sector, 'data_sector.csv')
+
+      return(data_sector)
+
+    }
+
+    # 개별종목 가치 지표
+    down_value = function(date) {
+      gen_otp_data = list(name = "fileDown", filetype = "csv",
+                          url = "MKD/13/1302/13020401/mkd13020401", market_gubun = 'ALL',
+                          gubun = '1', schdate = date,
+                          pagePath = "/contents/MKD/13/1302/13020401/MKD13020401.jsp")
+      otp = POST(gen_otp_url, query = gen_otp_data) %>%
+        read_html() %>% html_text()
+      down_data = list(code = otp)
       down = POST(down_url, query = down_data,
                   add_headers(referer = gen_otp_url)) %>%
         read_html() %>% html_text() %>% read_csv()
 
-      return(down)
+      data_value = down[, c(2, 5,7,9,11)]
+
+      data_value[, c(3:4)] = sapply(data_value[, c(3:4)], chr_to_numeric)
+      data_value$ROE = data_value$PBR / data_value$PER
+
+      write.csv(data_value, 'data_value.csv')
+      return(data_value)
+
     }
 
-    ticker = c("STK", "KSQ")
-    ticker_add = c("KS", "KQ")
     date = gsub("-", "", Sys.Date()-1)
 
-    for (i in 1:2) {
+    down_table = list()
 
-      down_table = c()
-
-      while (is.null(down_table)) {
-        tryCatch({
-          down_data(ticker[i], date)
-        }, error = function(e) {
-          date <<- as.character(as.numeric(date) - 1)
-        })
-        down_table = down_data(ticker[i], date)
-      }
-
-      down_table$market = ticker_add[i]
-      down_table = down_table[, -1] %>% data.frame()
-      data[[i]] = down_table
-
+    while (length(down_table) == 0) {
+      tryCatch({
+        down_value(date)
+      }, error = function(e) {
+        date <<- as.character(as.numeric(date) - 1)
+      })
+      down_table[[1]] = down_sector(date)
+      down_table[[2]] = down_value(date)
     }
 
-    data = do.call(rbind, data)
+    data = merge(down_table[[1]], down_table[[2]], by = '\uc885\ubaa9\ucf54\ub4dc')
+    data = data[order(-data['\uc2dc\uac00\ucd1d\uc561\u0028\uc6d0\u0029']), ]
 
   }
 
@@ -72,7 +99,7 @@ get_KOR_ticker = function(src = 'krx') {
 
     for (i in 0:1) {
 
-      mkt = ifelse(i == 0, "KS", "KQ")
+      mkt = ifelse(i == 0, "\ucf54\uc2a4\ud53c", "\ucf54\uc2a4\ub2e5")
 
       ticker = list()
       url = paste0("https://finance.naver.com/sise/sise_market_sum.nhn?sosok=",i,"&page=1")
@@ -128,6 +155,7 @@ get_KOR_ticker = function(src = 'krx') {
 
       ticker = do.call(rbind, ticker)
       ticker$market = mkt
+      ticker = ticker[, c(1,13,2,3,6,7,8,11,12)]
 
       data[[i + 1]] = ticker
       Sys.sleep(2)
@@ -137,21 +165,16 @@ get_KOR_ticker = function(src = 'krx') {
     data = do.call(rbind, data)
     data = data[which(data["\uc561\uba74\uac00"] != "0"), ]
 
-    numeric.point = c(3:4, 6:12)
-    data[,numeric.point] = sapply(data[,numeric.point], function(x) {
-      gsub(",", "", x) %>%
-        as.numeric()
-    })
+    data[, c(4:9)] = sapply(data[, c(4:9)], chr_to_numeric)
 
   }
 
   print("Data download is complete. Data cleansing is in progress.")
 
-  data = data[!grepl("\uc2a4\ud329", data[, 2]), ] # 스팩 종목 제외
-  data = data[substr(data[,2], nchar(data[,2]), nchar(data[,2])) != "\uc6b0", ] # 우선주 제외
-  data = data[substr(data[,2], nchar(data[,2]) - 1, nchar(data[,2])) != "\uc6b0\u0042", ] # 우선주 제외
-  data = data[substr(data[,2], nchar(data[,2]) - 1, nchar(data[,2])) != "\uc6b0\u0043", ] # 우선주 제외
-  data = data[substr(data[,2], nchar(data[,2]), nchar(data[,2])) != "\ud638", ] # 특별 종목 제외
+  data = data[!grepl("\uc2a4\ud329", data[, 3]), ] # 스팩 종목 제외
+  data = data[substr(data[,3], nchar(data[,3]), nchar(data[,3])) != "\uc6b0", ] # 우선주 제외
+  data = data[substr(data[,3], nchar(data[,3]) - 1, nchar(data[,3])) != "\uc6b0\u0042", ] # 우선주 제외
+  data = data[substr(data[,3], nchar(data[,3]) - 1, nchar(data[,3])) != "\uc6b0\u0043", ] # 우선주 제외
 
   rownames(data) = NULL
 
